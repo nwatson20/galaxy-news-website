16fae312e481ae4524ca6d8ea13234dcbdbba181
Nan <135907084+nwatson20@users.noreply.github.com>
2024-11-15 20:39:54 -0600

    Uploaded footer.php

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+	<p>Contact us through email <a href="mailto:nwatson20@student.alamo.edu">here</a> 
+	if you would like to contribute to the publication or if you have any inquiries.</p>
+	<p>Thank you for visiting our site!</p>
+	<p>Nanesko Watson</p>
+</footer>
